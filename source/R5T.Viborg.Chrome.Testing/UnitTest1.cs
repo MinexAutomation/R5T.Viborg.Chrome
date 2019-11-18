537a189cd3b0433b28b224d82a0a4d59a171619c
MinexAutomation <minex.automation@gmail.com>
2019-11-18 15:05:54 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Viborg.Chrome.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Viborg.Chrome.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Viborg.Chrome/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Viborg.Chrome
+{
+    public class Class1
+    {
+    }
+}
